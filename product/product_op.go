9f0fb9d0b6192e40448f3d1990ba79d70f5f6f6b
gura <csgura@gmail.com>
2023-09-15 17:51:26 +0900

    gen_product 삭제

--- product/product_op.go
+++ product/product_op.go
@@ -1,9 +1,10 @@
-//go:generate go run github.com/csgura/fp/internal/generator/product_gen
+//go:generate go run github.com/csgura/fp/internal/generator/template_gen
 package product
 
 import (
 	"github.com/csgura/fp"
 	"github.com/csgura/fp/as"
+	"github.com/csgura/fp/genfp"
 	"github.com/csgura/fp/hlist"
 )
 
@@ -46,4 +47,80 @@ func LiftValue[K, V, R any](mapf func(K, V) R) fp.Func1[fp.Tuple2[K, V], fp.Tupl
 	return func(a1 fp.Tuple2[K, V]) fp.Tuple2[K, R] {
 		return as.Tuple2(a1.I1, mapf(a1.I1, a1.I2))
 	}
+}
+
+// @internal.Generate
+var _ = genfp.GenerateFromUntil{
+	File: "tuple_gen.go",
+	Imports: []genfp.ImportPackage{
+		{Package: "github.com/csgura/fp", Name: "fp"},
+		{Package: "github.com/csgura/fp/hlist", Name: "hlist"},
+		{Package: "github.com/csgura/fp/as", Name: "as"},
+	},
+	From:  3,
+	Until: genfp.MaxProduct,
+	Template: `
+func Tuple{{.N}}[{{TypeArgs 1 .N}} any]({{DeclArgs 1 .N}}) fp.Tuple{{.N}}[{{TypeArgs 1 .N}}] {
+	return fp.Tuple{{.N}}[{{TypeArgs 1 .N}}]{
+		{{- range $idx := Range 1 .N}}
+			I{{$idx}}: a{{$idx}},
+		{{- end}}
+	}
+}
+	`,
+}
+
+// @internal.Generate
+var _ = genfp.GenerateFromUntil{
+	File: "tuple_gen.go",
+	Imports: []genfp.ImportPackage{
+		{Package: "github.com/csgura/fp", Name: "fp"},
+		{Package: "github.com/csgura/fp/hlist", Name: "hlist"},
+		{Package: "github.com/csgura/fp/as", Name: "as"},
+	},
+	From:  2,
+	Until: genfp.MaxProduct,
+	Template: `
+func TupleFromHList{{.N}}[{{TypeArgs 1 .N}} any](list {{ConsType 1 .N "hlist.Nil"}}) fp.Tuple{{.N}}[{{TypeArgs 1 .N}}] {
+	tail := TupleFromHList{{dec .N}}(list.Tail())
+	return Tuple{{.N}}(list.Head(), {{CallArgs 1 (dec .N) "tail.I"}})
+}
+	`,
+}
+
+// @internal.Generate
+var _ = genfp.GenerateFromUntil{
+	File: "tuple_gen.go",
+	Imports: []genfp.ImportPackage{
+		{Package: "github.com/csgura/fp", Name: "fp"},
+		{Package: "github.com/csgura/fp/hlist", Name: "hlist"},
+		{Package: "github.com/csgura/fp/as", Name: "as"},
+	},
+	From:  2,
+	Until: genfp.MaxProduct,
+	Template: `
+func LabelledFromHList{{.N}}[{{TypeArgs 1 .N}} fp.Named](list {{ConsType 1 .N "hlist.Nil"}}) fp.Labelled{{.N}}[{{TypeArgs 1 .N}}] {
+	tail := LabelledFromHList{{dec .N}}(list.Tail())
+	return as.Labelled{{.N}}(list.Head(), {{CallArgs 1 (dec .N) "tail.I"}})
+}
+	`,
+}
+
+// @internal.Generate
+var _ = genfp.GenerateFromUntil{
+	File: "tuple_gen.go",
+	Imports: []genfp.ImportPackage{
+		{Package: "github.com/csgura/fp", Name: "fp"},
+		{Package: "github.com/csgura/fp/hlist", Name: "hlist"},
+		{Package: "github.com/csgura/fp/as", Name: "as"},
+	},
+	From:  2,
+	Until: genfp.MaxProduct,
+	Template: `
+func Lift{{.N}}[{{TypeArgs 1 .N}}, R any](f func({{TypeArgs 1 .N}}) R) func(fp.Tuple{{.N}}[{{TypeArgs 1 .N}}]) R {
+	return func(t fp.Tuple{{.N}}[{{TypeArgs 1 .N}}]) R {
+		return f(t.Unapply())
+	}
+}
+	`,
 }
