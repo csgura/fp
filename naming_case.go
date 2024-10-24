7e13141d7452f0fb8b3695dcb4ee34f584bc0fa0
gura <csgura@gmail.com>
2024-10-24 17:41:54 +0900

    naming case

--- naming_case.go
+++ naming_case.go
@@ -12,13 +12,40 @@ const (
 	PascalCase NamingCase = "PascalCase"
 	SnakeCase  NamingCase = "snake_case"
 	KebabCase  NamingCase = "kebab-case"
+	HeaderCase NamingCase = "Header-Case"
 )
+
+func spanSlice[T any](s []T, p func(T) bool) ([]T, []T) {
+	left := []T{}
+	right := []T{}
+
+	span := false
+	for _, v := range s {
+		if span {
+			right = append(right, v)
+		} else {
+			if p(v) {
+				left = append(left, v)
+			} else {
+				span = true
+				right = append(right, v)
+			}
+		}
+	}
+
+	return left, right
+}
 
 func firstLower(name string) string {
 	if name == "" {
 		return ""
 	}
 	return strings.ToLower(name[:1]) + name[1:]
+}
+
+func firstSeriesLower(name string) string {
+	l, r := spanSlice([]rune(name), unicode.IsUpper)
+	return strings.ToLower(string(l)) + string(r)
 }
 
 func firstUpper(name string) string {
@@ -45,25 +72,72 @@ func foldSlice[A, B any](s []A, zero B, f func(B, A) B) B {
 func splitUpper(name string) Iterator[string] {
 
 	type buffer struct {
-		list    []string
-		builder *strings.Builder
+		list         []string
+		builder      *strings.Builder
+		includeLower bool
 	}
 
 	buf := foldSlice([]rune(name), buffer{builder: &strings.Builder{}}, func(b buffer, a rune) buffer {
-		if unicode.IsUpper(a) && b.builder.Len() > 0 {
-			nl := append(b.list, b.builder.String())
-			b.builder.Reset()
-			b.builder.WriteRune(a)
-			return buffer{
-				list:    nl,
-				builder: b.builder,
+		if unicode.IsUpper(a) {
+			if b.includeLower {
+				nl := append(b.list, b.builder.String())
+				b.builder.Reset()
+				b.builder.WriteRune(a)
+				return buffer{
+					list:    nl,
+					builder: b.builder,
+				}
+			} else {
+				b.builder.WriteRune(a)
 			}
+		} else {
+			b.includeLower = true
+			b.builder.WriteRune(a)
 		}
-		b.builder.WriteRune(a)
+
 		return b
 	})
 	ret := append(buf.list, buf.builder.String())
 	return IteratorOfSeq(ret)
+}
+
+func runeAt(v string, idx int) Option[rune] {
+	if idx < len(v) {
+		return Some(([]rune(v))[idx])
+	}
+	return None[rune]()
+}
+
+func isUpper(v string, idx int) bool {
+	r := runeAt(v, idx)
+	if r.IsDefined() {
+		return unicode.IsUpper(r.Get())
+	}
+	return false
+}
+
+func CheckNaming(name string) Option[NamingCase] {
+
+	none := None[NamingCase]()
+
+	if name == "" {
+		return none
+	}
+
+	if strings.Contains(name, "-") {
+		if strings.Contains(name, "_") {
+			return none
+		}
+		if isUpper(name, 0) {
+			return Some(HeaderCase)
+		}
+		return Some(KebabCase)
+	} else if strings.Contains(name, "_") {
+		return Some(SnakeCase)
+	} else if isUpper(name, 0) {
+		return Some(PascalCase)
+	}
+	return Some(CamelCase)
 }
 
 func ConvertNaming(name string, namingCase NamingCase) string {
@@ -71,15 +145,33 @@ func ConvertNaming(name string, namingCase NamingCase) string {
 		return ""
 	}
 
+	inferred := CheckNaming(name)
+
 	switch namingCase {
 	case CamelCase:
-		return firstLower(splitSnakeAndKebab(name).Map(firstUpper).MakeString(""))
+		switch inferred {
+		case Some(KebabCase), Some(SnakeCase), Some(HeaderCase):
+			return firstSeriesLower(splitSnakeAndKebab(name).Map(firstUpper).MakeString(""))
+		case Some(PascalCase):
+			return firstSeriesLower(name)
+		default:
+			return name
+		}
 	case PascalCase:
-		return splitSnakeAndKebab(name).Map(firstUpper).MakeString("")
+		switch inferred {
+		case Some(KebabCase), Some(SnakeCase), Some(HeaderCase):
+			return splitSnakeAndKebab(name).Map(firstUpper).MakeString("")
+		case Some(CamelCase):
+			return firstUpper(name)
+		default:
+			return name
+		}
 	case SnakeCase:
 		return splitSnakeAndKebab(name).FlatMap(splitUpper).Map(strings.ToLower).MakeString("_")
 	case KebabCase:
 		return splitSnakeAndKebab(name).FlatMap(splitUpper).Map(strings.ToLower).MakeString("-")
+	case HeaderCase:
+		return splitSnakeAndKebab(name).FlatMap(splitUpper).Map(firstUpper).MakeString("-")
 	default:
 		return name
 	}
--- test/naming_test.go
+++ test/naming_test.go
@@ -5,23 +5,42 @@ import (
 
 	"github.com/csgura/fp"
 	"github.com/csgura/fp/internal/assert"
+	"github.com/csgura/fp/option"
 )
 
 func TestNaming(t *testing.T) {
 
+	assert.Equal(fp.CheckNaming("HelloWorldTowerCrain"), option.Some(fp.PascalCase))
 	assert.Equal(fp.ConvertNaming("HelloWorldTowerCrain", fp.CamelCase), "helloWorldTowerCrain")
 	assert.Equal(fp.ConvertNaming("hello-world-tower-crain", fp.CamelCase), "helloWorldTowerCrain")
 	assert.Equal(fp.ConvertNaming("hello_world_tower_crain", fp.CamelCase), "helloWorldTowerCrain")
+	assert.Equal(fp.ConvertNaming("Hello-World-Tower-Crain", fp.CamelCase), "helloWorldTowerCrain")
 
 	assert.Equal(fp.ConvertNaming("helloWorldTowerCrain", fp.PascalCase), "HelloWorldTowerCrain")
 	assert.Equal(fp.ConvertNaming("hello_world_tower_crain", fp.PascalCase), "HelloWorldTowerCrain")
 	assert.Equal(fp.ConvertNaming("hello-world-tower-crain", fp.PascalCase), "HelloWorldTowerCrain")
+	assert.Equal(fp.ConvertNaming("Hello-World-Tower-Crain", fp.PascalCase), "HelloWorldTowerCrain")
 
 	assert.Equal(fp.ConvertNaming("helloWorldTowerCrain", fp.SnakeCase), "hello_world_tower_crain")
 	assert.Equal(fp.ConvertNaming("HelloWorldTowerCrain", fp.SnakeCase), "hello_world_tower_crain")
 	assert.Equal(fp.ConvertNaming("hello-world-tower-crain", fp.SnakeCase), "hello_world_tower_crain")
+	assert.Equal(fp.ConvertNaming("Hello-World-Tower-Crain", fp.SnakeCase), "hello_world_tower_crain")
 
 	assert.Equal(fp.ConvertNaming("helloWorldTowerCrain", fp.KebabCase), "hello-world-tower-crain")
 	assert.Equal(fp.ConvertNaming("HelloWorldTowerCrain", fp.KebabCase), "hello-world-tower-crain")
 	assert.Equal(fp.ConvertNaming("hello_world_tower_crain", fp.KebabCase), "hello-world-tower-crain")
+	assert.Equal(fp.ConvertNaming("Hello-World-Tower-Crain", fp.KebabCase), "hello-world-tower-crain")
+
+	assert.Equal(fp.ConvertNaming("helloWorldTowerCrain", fp.HeaderCase), "Hello-World-Tower-Crain")
+	assert.Equal(fp.ConvertNaming("HelloWorldTowerCrain", fp.HeaderCase), "Hello-World-Tower-Crain")
+	assert.Equal(fp.ConvertNaming("hello_world_tower_crain", fp.HeaderCase), "Hello-World-Tower-Crain")
+	assert.Equal(fp.ConvertNaming("hello-world-tower-crain", fp.HeaderCase), "Hello-World-Tower-Crain")
+
+	assert.Equal(fp.ConvertNaming("PDUSessionChargingInformation", fp.HeaderCase), "PDUSession-Charging-Information")
+	assert.Equal(fp.ConvertNaming("PDUSessionChargingInformation", fp.KebabCase), "pdusession-charging-information")
+	assert.Equal(fp.ConvertNaming("PDUSessionChargingInformation", fp.CamelCase), "pdusessionChargingInformation")
+
+	assert.Equal(fp.ConvertNaming("PDUSession-Charging-Information", fp.CamelCase), "pdusessionChargingInformation")
+	assert.Equal(fp.ConvertNaming("PDUSession-Charging-Information", fp.PascalCase), "PDUSessionChargingInformation")
+
 }
