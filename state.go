1270f914fd42d37fee0e69642c68d6a8047ca3b3
gura <csgura@gmail.com>
2024-11-01 14:01:33 +0900

    state 주석

--- state.go
+++ state.go
@@ -1,5 +1,9 @@
 package fp
 
+// haskell 에서 StateT 타입은   s -> m ( a,s ) 타입이지만
+// s 까지  m 안에 있는 것은 불편할 때가 더 많기 때문에
+// ( 에러 발생시  에러에  대한 것을 s안에 기록하기 힘듬 )
+// s -> (m a , s )  형태로 정의
 type StateT[S, A any] func(S) (Try[A], S)
 
 func (r StateT[S, A]) Run(s S) (Try[A], S) {
