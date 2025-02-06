624d652353762351a2af6bf3f6a93a194b509290
gura <csgura@gmail.com>
2025-02-06 14:44:13 +0900

    mshow diet

--- mshow/mshow.go
+++ mshow/mshow.go
@@ -222,8 +222,12 @@ func trailingComma(opt fp.ShowOption) string {
 func Seq[T any](tshow Show[T]) Show[fp.Seq[T]] {
 	return NewAppend(func(buf []string, s fp.Seq[T], opt fp.ShowOption) []string {
 		childOpt := opt.IncreaseIndent()
-		childStr := iterator.Map(iterator.FromSeq(s), fp.Flip(as.Curried3(tshow.Append)(nil))(childOpt))
-		return appendSeq(buf, "Seq", childStr, opt)
+
+		var childStr [][]string
+		for _, v := range s {
+			childStr = append(childStr, tshow.Append(nil, v, childOpt))
+		}
+		return appendSeq(buf, "Seq", iterator.FromSlice(childStr), opt)
 	})
 }
 
@@ -289,8 +293,10 @@ func GoMap[K comparable, V any](showk Show[K], showv Show[V]) Show[map[K]V] {
 }
 
 func Slice[T any](tshow Show[T]) Show[[]T] {
-	return ContraMap(Seq(tshow), func(u []T) fp.Seq[T] {
-		return u
+	return NewAppend(func(buf []string, s []T, opt fp.ShowOption) []string {
+		childOpt := opt.IncreaseIndent()
+		childStr := iterator.Map(iterator.FromSeq(s), fp.Flip(as.Curried3(tshow.Append)(nil))(childOpt))
+		return appendSeq(buf, "Seq", childStr, opt)
 	})
 }
 
