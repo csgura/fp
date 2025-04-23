e96b0cdc4239fd60a0b51a6de3d4079d875cf90f
gura <csgura@gmail.com>
2025-04-23 11:02:47 +0900

    xtr 에 이것저것 추가

--- xtr/xtr.go
+++ xtr/xtr.go
@@ -1,5 +1,7 @@
 package xtr
 
+import "github.com/csgura/fp/genfp"
+
 // head([ 1,2,3 ] ) ->  1
 func Head[T interface{ Head() V }, V any](t T) V {
 	return t.Head()
@@ -18,4 +20,19 @@ func Last[T interface{ Last() V }, V any](t T) V {
 // tail([1,2,3]) -> [2,3]
 func Tail[T interface{ Tail() V }, V any](t T) V {
 	return t.Tail()
+}
+
+//go:generate go run github.com/csgura/fp/internal/generator/template_gen
+
+// @internal.Generate
+var _ = genfp.GenerateFromList{
+	File: "xtr_gen.go",
+	List: []string{
+		"List", "Data", "DataList", "Response", "Get", "Left", "Key", "Value", "Meta", "ID", "Id",
+	},
+	Template: `
+		func {{.N}}[T interface{ {{.N}}() V }, V any](t T) V {
+			return t.{{.N}}()
+		}
+	`,
 }
