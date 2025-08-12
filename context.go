4dc97bb15477642e0b088c18db5abba79902aa7a
gura <csgura@gmail.com>
2025-08-12 13:41:57 +0900

    context nil check

--- context.go
+++ context.go
@@ -16,12 +16,18 @@ func (r ValueType[T]) ZeroValue() T {
 
 func WithContextValue[K TypedKey[T], T any](ctx context.Context, value T) context.Context {
 	var key K
+	if ctx == nil {
+		ctx = context.Background()
+	}
 	return context.WithValue(ctx, key, value)
 }
 
 func GetContextValue[K TypedKey[T], T any](ctx context.Context) Option[T] {
 	var key K
 
+	if ctx == nil {
+		return None[T]()
+	}
 	if ret, ok := ctx.Value(key).(T); ok {
 		return Some(ret)
 	}
