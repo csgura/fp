88a5f0edf6bb6408756546740025222c9584e43f
gura <csgura@gmail.com>
2025-10-23 17:31:14 +0900

    sorted map

--- slice/sorted_map.go
+++ slice/sorted_map.go
@@ -1,6 +1,9 @@
 package slice
 
-import "github.com/csgura/fp"
+import (
+	"github.com/csgura/fp"
+	"github.com/csgura/fp/ord"
+)
 
 type SortedMap[K, V any] struct {
 	ord    fp.Ord[K]
@@ -11,13 +14,13 @@ func (r SortedMap[K, V]) Size() int {
 	return len(r.sorted)
 }
 
-func bsearch[K, V any](s []fp.Tuple2[K, V], ord fp.Ord[K], k K) int {
+func bsearch[V any](s []V, ord fp.Ord[V], k V) int {
 	low := 0
 	high := len(s) - 1
 
 	for low <= high {
 		mid := (low + high) / 2
-		midVal := s[mid].I1
+		midVal := s[mid]
 
 		cmp := ord.Compare(midVal, k)
 		if cmp < 0 {
@@ -31,9 +34,15 @@ func bsearch[K, V any](s []fp.Tuple2[K, V], ord fp.Ord[K], k K) int {
 	return -(low + 1) // key not found.
 }
 
+func bsearchKey[K, V any](s []fp.Tuple2[K, V], ordk fp.Ord[K], k K) int {
+	return bsearch(s, ord.Key[K, V](ordk), fp.Tuple[K, V]{
+		I1: k,
+	})
+}
+
 // Returns a view of the portion of this map whose keys are greater than or equal to fromKey
 func (r SortedMap[K, V]) TailMap(fromKey K) SortedMap[K, V] {
-	idx := bsearch(r.sorted, r.ord, fromKey)
+	idx := bsearchKey(r.sorted, r.ord, fromKey)
 	if idx < 0 {
 		idx = -(idx + 1)
 	}
@@ -44,7 +53,7 @@ func (r SortedMap[K, V]) TailMap(fromKey K) SortedMap[K, V] {
 }
 
 func (r SortedMap[K, V]) Get(k K) fp.Option[V] {
-	idx := bsearch(r.sorted, r.ord, k)
+	idx := bsearchKey(r.sorted, r.ord, k)
 	if idx < 0 {
 		return fp.None[V]()
 	}
@@ -53,4 +62,14 @@ func (r SortedMap[K, V]) Get(k K) fp.Option[V] {
 
 func (r SortedMap[K, V]) Iterator() fp.Iterator[fp.Tuple2[K, V]] {
 	return fp.IteratorOfSeq(r.sorted)
+}
+
+func (r SortedMap[K, V]) All() fp.GoIter[fp.Tuple2[K, V]] {
+	return func(yield func(fp.Tuple2[K, V]) bool) {
+		for _, v := range r.sorted {
+			if !yield(v) {
+				return
+			}
+		}
+	}
 }
